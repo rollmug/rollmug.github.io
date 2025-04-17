294eed011f6540df079a78b1cb1601bdf4a55c52
Chris Welbon <chrisw@rlmg.com>
2025-04-17 19:17:47 -0400

    are we pushing Docusaurus finally?

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/rlmg/wiki/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/rlmg/wiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/rlmg/wiki/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/rlmg/wiki/src/css/custom.css"),
+];
